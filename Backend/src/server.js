598186a955a6cb5adc22ae7c0af539204061ee01
Dev Shah <busycaesar@gmail.com>
2024-02-23 17:12:44 -0500

    Chore(B): Install cors and configured to accept requests from PORT 5173!

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
 app.get("/", (req, res) => {
   console.log("Received GET/ request!");
