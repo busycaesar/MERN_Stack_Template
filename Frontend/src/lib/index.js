83ef4c618ccfe14996bcfbd5b1efc4f9e5c620cc
Dev Shah <busycaesar@gmail.com>
2024-02-23 17:10:59 -0500

    Add(F): Add lib directory to store functions use to fetch data from backend!

--- Frontend/src/lib/index.js
+++ Frontend/src/lib/index.js
@@ -1,3 +1,10 @@
+import axios from "axios";
+
 /**
  * This directory contains all the functions for fetching the data from backend using API.
  */
+export const getRoot = async () => {
+  const response = await axios.get(`${import.meta.env.VITE_BACKEND_DOMAIN}/`);
+  if (response.status == 200) return response.data;
+  else throw new Error("Server Error!");
+};
