e98a8556bebdefadc4698e28f37eb05d89ee1082
Dev Shah <busycaesar@gmail.com>
2024-02-24 14:08:08 -0500

    Refactor(B): Separated app and routes from server for modularization.

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,20 +1,7 @@
-const express = require("express");
-const app = express();
+const app = require("./app");
 require("dotenv").config();
 const PORT = process.env.PORT || 8080;
-const cors = require("cors");
 const { connectMongoDB } = require("./Database");
-
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
-
-app.get("/", (req, res) => {
-  console.log("Received GET/ request!");
-  res.status(200).json("Hello from backend!");
-});
 
 connectMongoDB()
   .then(app.listen(PORT, () => console.log(`Server started on PORT ${PORT}!`)))
